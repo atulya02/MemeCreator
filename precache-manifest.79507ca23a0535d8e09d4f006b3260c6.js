d0249ccb584a1c8fb5d8798444e04aaeda672e84
Atulya Raj <atulya.raj2904@gmail.com>
2019-06-12 16:09:57 +0530

    Updates

---
+++ precache-manifest.79507ca23a0535d8e09d4f006b3260c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d881e8a0bdbbfe5f84c632471971601c",
+    "url": "/MemeCreator/index.html"
+  },
+  {
+    "revision": "53e05f5e53b477ff8fb2",
+    "url": "/MemeCreator/static/css/main.25471e83.chunk.css"
+  },
+  {
+    "revision": "f49f49a68cae45f36850",
+    "url": "/MemeCreator/static/js/2.7be4d342.chunk.js"
+  },
+  {
+    "revision": "53e05f5e53b477ff8fb2",
+    "url": "/MemeCreator/static/js/main.360e6855.chunk.js"
+  },
+  {
+    "revision": "bafceecc21620deca69b",
+    "url": "/MemeCreator/static/js/runtime~main.d2b5f1a0.js"
+  }
+]);
